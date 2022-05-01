8a3b982d0255e8bc6a7c099aed46a47a1cbf39eb
enrique reichel <quiqueapi@gmail.com>
2022-05-01 19:25:05 -0300

    homework terminada

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -74,15 +74,15 @@ function colors(color) {
   
   switch (color) {
     case "blue": 
-      return "this is blue";
-    case "blue": 
-      return "this is red";
-    case "blue": 
-      return "this is green";
-    case "blue": 
-      return "this is orange";
+      return "This is blue";
+    case "red": 
+      return "This is red";
+    case "green": 
+      return "This is green";
+    case "orange": 
+      return "This is orange";
     default: 
-      return "color not found";
+      return "Color not found";
   }
 }
 
@@ -142,11 +142,21 @@ function operadoresLogicos(num1, num2, num3) {
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
 
-if (num1 < 0 || num2 < 0 || num3 < 0) return "hay negativos";
-if (num1 === 0 || num2 === 0 || num3 === 0) return "Error";
-if (num1 > num2 && num1 > num3 && num1 > 0) return "Número 1 es mayor y positivo";
-if (num3 > num1 && num3 > num2) return ++num3;
-return "faslse";
+  if(num1 < 0 || num2 < 0 || num3 < 0) {
+    return "Hay negativos";
+  }
+  else if(num1 === 0 || num2 === 0 || num3 === 0) {
+    return "Error";
+  }
+  else if(num1 > 0 && num1 > num2 && num1 > num3) {
+    return "Número 1 es mayor y positivo";
+  }
+  else if(num3 > num1 && num3 > num2) {
+    return num3 + 1;
+  }
+  else {
+    return false;
+  }
 }
 
 function esPrimo(numero) {
@@ -156,22 +166,22 @@ function esPrimo(numero) {
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
 
-if (numero < 2) return false
-if (numero === 2) return true
-for(var i = 2 ; i < numero; i++) {
-  if (numero % i === 0) {
-    return false
+  if( numero < 2) return false;
+  if(numero === 2) return true;
+  for(var i = 2; i < numero; i++) {
+    if(numero % i === 0) {
+      return false;
+    }
   }
-}
-return true
+  return true;
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-if (valor ===true) return "soy verdadero";
-return "soy falso"
+if (valor ===true) return "Soy verdadero";
+return "Soy falso"
 }
 
 function tablaDelSeis(){
@@ -179,10 +189,11 @@ function tablaDelSeis(){
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
   
-  var TablaDelSeis = []
-  for (var i = 0 ; 1 < 11; i++) {
-    ArraytablaDelSeis.push (6 * i)
+  let arrayTablaDel6 = []
+  for (let i = 0; i < 11; i++) {
+        arrayTablaDel6.push(6 * i)
   }
+  return arrayTablaDel6
 }
 
 function tieneTresDigitos(numero){
